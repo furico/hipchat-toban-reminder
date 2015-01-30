059a8c655e8dacf69eee050a578096e4b1a8e3e5
Hiroyuki Furihata <furihatah@gmail.com>
2015-01-30 23:33:25 +0900

    add_mark のテストを作成

--- test_reminder.py
+++ test_reminder.py
@@ -1,10 +1,13 @@
 """Tests for Reminder."""
 
 import unittest
+import reminder
 
 
 class ReminderTestCase(unittest.TestCase):
-    pass
+    def test_add_atmark(self):
+        result = reminder.add_atmark('test')
+        self.assertEqual('@test', result)
 
 if __name__ == '__main__':
     unittest.main()
