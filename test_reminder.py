773492f9886b5e4155ae843b98e5ccec90380188
Hiroyuki Furihata <furihatah@gmail.com>
2015-01-31 21:13:45 +0900

    arrange_listのテストを作成

--- test_reminder.py
+++ test_reminder.py
@@ -9,5 +9,19 @@ class ReminderTestCase(unittest.TestCase):
         result = reminder.add_atmark('test')
         self.assertEqual('@test', result)
 
+    def test_arrange_list(self):
+        ls = [1, 2, 3, 4, 5]
+        index = 0
+        result = reminder.arrange_list(ls, index)
+        self.assertEqual([1, 2, 3, 4, 5], result)
+
+        index = 2
+        result = reminder.arrange_list(ls, index)
+        self.assertEqual([3, 4, 5, 1, 2], result)
+
+        index = len(ls) - 1
+        result = reminder.arrange_list(ls, index)
+        self.assertEqual([5, 1, 2, 3, 4], result)
+
 if __name__ == '__main__':
     unittest.main()
